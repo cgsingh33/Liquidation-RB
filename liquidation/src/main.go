5050e348ab27ae51af58adddb980a32714f72488
Chandragupta Singh <chandragupta.singh4@gmail.com>
2024-03-24 02:26:40 +0530

    get user addressess code refactored

--- liquidation/src/main.go
+++ liquidation/src/main.go
@@ -13,6 +13,7 @@ import (
 	libclient "github.com/tendermint/tendermint/rpc/jsonrpc/client"
 	"net/http"
 	"os"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -35,43 +36,64 @@ func main() {
 	rpc := os.Getenv("RPC_ENDPOINT")
 	contract := os.Getenv("REDBANK_CONTRACT")
 
-	address := "osmo1m8sxn5fnn74ftw2npw5d2fu4gxlha64ttur36x"
-	debtCoins := []Coin{}
-	collateralCoins := []Coin{}
-
-	wg.Add(3)
-
 	// Get all addresses
-	go getAllAddresses(contract, rpc, &wg)
+	redBankUsers, _, err := getAllAddresses(contract, rpc)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 
-	// Get user debt positions
-	go func() {
-		defer wg.Done()
-		debtCoins, err = userPosition(rest, contract, "user_debts", address)
-		if err != nil {
-			fmt.Println("Error:", err)
-			return
+	for _, address := range redBankUsers {
+		fmt.Println("user address: ", address)
+
+		debtCoins := []Coin{}
+		collateralCoins := []Coin{}
+
+		// Create a wait group
+		var wg sync.WaitGroup
+		wg.Add(3)
+
+		// Launch goroutines for debt and collateral positions
+		go func() {
+			defer wg.Done()
+			debtCoins, err = userPosition(rest, contract, "user_debts", address)
+			if err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
+		}()
+
+		go func() {
+			defer wg.Done()
+			collateralCoins, err = userPosition(rest, contract, "user_collaterals", address)
+			if err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
+		}()
+
+		go func() {
+			defer wg.Done()
+			err = userTotalPosition(rest, contract, "user_position_liquidation_pricing", address)
+			if err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
+		}()
+
+		wg.Wait()
+
+		fmt.Println("debtCoins: ", debtCoins, "collateralCoins: ", collateralCoins)
+
+		collateralizationRatio := calculateCollateralizationRatio(debtCoins, collateralCoins)
+
+		if len(debtCoins) == 0 {
+			fmt.Println("Collateralization Ratio N/A as zero debt")
+		} else {
+			fmt.Println("Collateralization Ratio: ", collateralizationRatio)
 		}
-		fmt.Println("Debts:", debtCoins)
-	}()
-
-	// Get user collateral positions
-	go func() {
-		defer wg.Done()
-		collateralCoins, err = userPosition(rest, contract, "user_collaterals", address)
-		if err != nil {
-			fmt.Println("Error:", err)
-			return
-		}
-		fmt.Println("Collaterals:", collateralCoins)
-	}()
-
-	wg.Wait()
-
-	// calculate collateralization ratio
-	collateralizationRatio := calculateCollateralizationRatio(debtCoins, collateralCoins)
-
-	fmt.Println("Collateralization Ratio:", collateralizationRatio)
+		fmt.Println("---------------------------------------------------")
+	}
 
 }
 
@@ -93,9 +115,10 @@ func calculateCollateralizationRatio(debtCoins, collateralCoins []Coin) sdkmath.
 		totalCollateral = totalCollateral.Add(collateral.Amount.Mul(price))
 	}
 
-	// calculate the collateralization ratio
-
-	collateralizationRatio := sdkmath.LegacyNewDecFromInt((totalCollateral.Quo(totalDebt)))
+	if totalDebt.IsZero() {
+		return sdkmath.LegacyNewDec(0)
+	}
+	collateralizationRatio := sdkmath.LegacyNewDecFromInt(totalCollateral).Quo(sdkmath.LegacyNewDecFromInt(totalDebt))
 
 	return collateralizationRatio.Quo(sdkmath.LegacyNewDec(100))
 
@@ -105,6 +128,53 @@ func getPrice(denom string) sdkmath.Int {
 	// get the price of the denom
 	// for now, we will return 1 as the price, but this will come from an oracle
 	return sdkmath.NewInt(1)
+}
+
+func userTotalPosition(rest, contract, queryType, address string) error {
+
+	query := fmt.Sprintf(`{"%s": {"user": "%s"}}`, queryType, address)
+
+	base64Encode := base64.StdEncoding.EncodeToString([]byte(query))
+
+	url := rest + "/cosmwasm/wasm/v1/contract/" + contract + "/smart/" + base64Encode
+
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return err
+	}
+	defer response.Body.Close()
+
+	var body map[string]interface{}
+
+	err = json.NewDecoder(response.Body).Decode(&body)
+	if err != nil {
+		return err
+	}
+
+	if item, ok := body["data"].(interface{}); ok {
+		if dataItem, ok := item.(map[string]interface{}); ok {
+			health_status := dataItem["health_status"]
+			if !ok {
+				fmt.Println("Error: Unable to get health status")
+				return nil
+			}
+			total_collateralized_debt, ok := dataItem["total_collateralized_debt"].(string)
+			if !ok {
+				return nil
+			}
+			total_enabled_collateral, ok := dataItem["total_enabled_collateral"].(string)
+			if !ok {
+				return nil
+			}
+			fmt.Println("Health Status: ", health_status)
+			fmt.Println("Total Collateralized Debt: ", total_collateralized_debt)
+			fmt.Println("Total Enabled Collateral: ", total_enabled_collateral)
+		}
+
+	}
+	return nil
+
 }
 
 func userPosition(rest, contract, queryType, address string) ([]Coin, error) {
@@ -154,12 +224,9 @@ func userPosition(rest, contract, queryType, address string) ([]Coin, error) {
 
 }
 
-// /////////////  WIP /////////////////
-func getAllAddresses(contract, rpc string, wg *sync.WaitGroup) ([][]byte, int64, error) {
+func getAllAddresses(contract, rpc string) ([]string, int64, error) {
 
-	defer wg.Done()
-
-	var results [][]byte
+	results := []string{}
 	var totalScanned int64
 
 	client, err := NewRPCClient(rpc, time.Second*30)
@@ -190,10 +257,75 @@ func getAllAddresses(contract, rpc string, wg *sync.WaitGroup) ([][]byte, int64,
 		return results, totalScanned, err
 	}
 
-	// fmt.Println(stateResponse.Models)
+	accounts := make(map[string]HealthCheckWorkItem)
+
+	for _, model := range stateResponse.Models {
+
+		totalScanned++
+
+		hexKey := model.Key
+		if len(hexKey) < 50 {
+			// Anything shorter than 50 can't be a map
+			continue
+		}
+
+		lengthIndicator := hexKey[0:2]
+		length, err := strconv.ParseInt(lengthIndicator.String(), 16, 64)
+		if err != nil {
+			fmt.Println("Error: Unable to decode contract state key (map name) ", err)
+			continue
+		}
+		// Shift to next section
+		hexKey = hexKey[2:]
+		// Shift to next section
+		hexKey = hexKey[length:]
+
+		// Determine the length of the address
+		lengthIndicator = hexKey[0:2]
+		length, err = strconv.ParseInt(lengthIndicator.String(), 16, 64)
+		if err != nil {
+			fmt.Println("Error: Unable to decode contract state key (address) ", err)
+			continue
+		}
+		// Shift to next section
+		hexKey = hexKey[2:]
+		// Address is next
+		address := hexKey[0:length]
+		// Shift to next section
+		hexKey = hexKey[length:]
+
+		identifier := string(address)
+
+		if _, ok := accounts[identifier]; !ok {
+			// Not added yet, init
+			accounts[identifier] = HealthCheckWorkItem{
+				Identifier: identifier,
+			}
+		}
+	}
+
+	for _, workItem := range accounts {
+		// Unmarshal the JSON string into the struct
+		err := json.Unmarshal([]byte(workItem.Identifier), &data)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return results, totalScanned, err
+		}
+		results = append(results, data.Addr)
+
+	}
+	// fmt.Println("Addresses:", results)
 
 	return results, totalScanned, nil
 
+}
+
+type HealthCheckWorkItem struct {
+	Identifier string `json:"identifier"`
+}
+
+var data struct {
+	Addr string `json:"addr"`
 }
 
 func NewRPCClient(addr string, timeout time.Duration) (*rpchttp.HTTP, error) {
